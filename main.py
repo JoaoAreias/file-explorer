fa2518d84ebd7b4d5718a2a40d7d983699f5e4d5
Joao Moraes <joaoareiasmoraes@gmail.com>
2026-03-25 05:13:40 -0300

    Show hidden folders and add sorting by folder size

--- main.py
+++ main.py
@@ -42,13 +42,18 @@ class FileListView(FileChooserListView):
         self._executor = ThreadPoolExecutor(max_workers=workers)
         self._pending_futures = []
         self._generation = 0
+        self._size_cache = {}
+        self._resorting = False
+        self._default_sort_func = self.sort_func
         self.bind(on_entry_added=self.update_entry)
 
     def _update_files(self, *args, **kwargs):
-        self._generation += 1
-        for future in self._pending_futures:
-            future.cancel()
-        self._pending_futures.clear()
+        if not self._resorting:
+            self._generation += 1
+            for future in self._pending_futures:
+                future.cancel()
+            self._pending_futures.clear()
+            self._size_cache.clear()
         super()._update_files(*args, **kwargs)
 
     def format_size(self, size):
@@ -67,18 +72,21 @@ class FileListView(FileChooserListView):
         """Updates the size of the file or folder."""
         if not os.path.isdir(entry.path):
             return
-        if not os.access(entry.path, os.W_OK):
-            return
 
         box_layout = entry.children[0]
         size_label = box_layout.children[0]
-        size_label.text = "..."
 
+        if entry.path in self._size_cache:
+            size_label.text = self.format_size(self._size_cache[entry.path])
+            return
+
+        size_label.text = "..."
         path = entry.path
         generation = self._generation
 
         def compute():
             size = self.size_provider(path)
+            self._size_cache[path] = size
             formatted = self.format_size(size)
             def update(dt):
                 if self._generation == generation:
@@ -87,8 +95,19 @@ class FileListView(FileChooserListView):
 
         self._pending_futures.append(self._executor.submit(compute))
 
+    def set_sort(self, by_size):
+        if by_size:
+            self.sort_func = lambda files, fs: sorted(
+                files, key=lambda f: self._size_cache.get(f, 0), reverse=True
+            )
+        else:
+            self.sort_func = self._default_sort_func
+        self._resorting = True
+        self._update_files()
+        self._resorting = False
+
 class ToolbarWidget(BoxLayout):
-    def __init__(self, on_select=None, on_refresh=None, **kwargs):
+    def __init__(self, on_select=None, on_refresh=None, on_sort=None, **kwargs):
         kwargs.setdefault('orientation', 'horizontal')
         kwargs.setdefault('size_hint', (1, 0.05))
         super().__init__(**kwargs)
@@ -96,15 +115,24 @@ class ToolbarWidget(BoxLayout):
         self.folder_name = TextInput(text='C:\\Users\\', multiline=False)
         select_button = Button(text='Select Folder')
         refresh_button = Button(text='Refresh')
+        self._sort_by_size = False
+        self._sort_button = Button(text='Sort: Name')
 
         if on_select:
             select_button.bind(on_press=lambda _: on_select(self.folder_name.text))
         if on_refresh:
             refresh_button.bind(on_press=lambda _: on_refresh())
+        if on_sort:
+            def toggle_sort(_):
+                self._sort_by_size = not self._sort_by_size
+                self._sort_button.text = 'Sort: Size' if self._sort_by_size else 'Sort: Name'
+                on_sort(self._sort_by_size)
+            self._sort_button.bind(on_press=toggle_sort)
 
         self.add_widget(self.folder_name)
         self.add_widget(select_button)
         self.add_widget(refresh_button)
+        self.add_widget(self._sort_button)
 
     @property
     def path(self):
@@ -122,8 +150,9 @@ class FolderExplorer(App):
         self.toolbar = ToolbarWidget(
             on_select=self.select_folder,
             on_refresh=self.refresh,
+            on_sort=self.change_sort,
         )
-        self.file_list_view = FileListView(size_provider=get_directory_size)
+        self.file_list_view = FileListView(size_provider=get_directory_size, show_hidden=True)
         self.file_list_view.bind(path=self.update_text_input)
 
         layout.add_widget(self.toolbar)
@@ -139,6 +168,9 @@ class FolderExplorer(App):
     def update_text_input(self, *args):
         """Updates the text input with the current folder."""
         self.toolbar.path = self.file_list_view.path
+
+    def change_sort(self, by_size):
+        self.file_list_view.set_sort(by_size)
 
     def refresh(self):
         """Invalidates the cache and refreshes the file list."""
